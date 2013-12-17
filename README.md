42fe406f3f18355a8cd74735ba7e31123da9edc2
Matthew Congrove <mattcongrove@gmail.com>
2013-12-17 01:53:52 -0500

    Porting to 2.0

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 #"Mask" for Pebble
-
+ 
 A Pebble watch face based on the [Mask](http://www.slvczch.com/mask-watch) watch by Filip Slovacek.
-
-![PebbleExtraNormal](https://raw.github.com/mcongrove/PebbleMask/master/watchface.jpg)
+ 
+![PebbleExtraNormal](https://raw.github.com/mcongrove/PebbleMask/master/watchface.png)
--- src/mask.c
+++ src/mask.c
@@ -22,19 +22,15 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include <pebble.h>
 
-#define MY_UUID { 0x6D, 0xB9, 0x1B, 0x59, 0x28, 0x59, 0x44, 0x59, 0xB3, 0x7D, 0xB7, 0x0D, 0x40, 0x04, 0xBB, 0x68 }
-
-PBL_APP_INFO(MY_UUID, "Mask", "Matthew Congrove", 1, 0, RESOURCE_ID_IMAGE_MENU_ICON, APP_INFO_WATCH_FACE);
-
-Window window;
-GRect AnalogueGRect;
-BmpContainer numbers_image;
-Layer minute_overlay_layer, hour_overlay_layer;
-GPath minute_overlay_path, hour_overlay_path;
+Window *window;
+Layer *minute_overlay_layer;
+Layer *hour_overlay_layer;
+static GPath *minute_overlay_path;
+static GPath *hour_overlay_path;
+GBitmap *numbers_image;
+BitmapLayer *numbers_layer;
 
 const GPathInfo MINUTE_OVERLAY_POINTS = {
 	4,
@@ -55,96 +51,98 @@ const GPathInfo HOUR_OVERLAY_POINTS = {
 	}
 };
 
-void init_layer_path_and_center(Layer *layer, GPath *path, const GPathInfo *pathInfo, const void *updateProc) {
-	layer_init(layer, AnalogueGRect);
-	layer->update_proc = updateProc;
-	
-	gpath_init(path, pathInfo);
-	gpath_move_to(path, grect_center_point(&layer->frame));
-}
-
-void minute_display_layer_update_callback(Layer *me, GContext* ctx) {
-	(void)me;
-
-	PblTm t;
-	get_time(&t);
-
-	unsigned int angle = ((t.tm_min - (t.tm_min % 5)) * 6);
-	gpath_rotate_to(&minute_overlay_path, (TRIG_MAX_ANGLE / 360) * angle);
-	
-	graphics_context_set_fill_color(ctx, GColorWhite);
-	graphics_fill_circle(ctx, grect_center_point(&me->frame), 44);
-	
-	graphics_context_set_fill_color(ctx, GColorBlack);
-	gpath_draw_filled(ctx, &minute_overlay_path);
-}
-
-void hour_display_layer_update_callback(Layer *me, GContext* ctx) {
-	(void)me;
-
-	PblTm t;
-	get_time(&t);
-
-	unsigned int angle = (t.tm_hour * 30);
-	gpath_rotate_to(&hour_overlay_path, (TRIG_MAX_ANGLE / 360) * angle);
-	
-	graphics_context_set_fill_color(ctx, GColorWhite);
-	graphics_fill_circle(ctx, grect_center_point(&me->frame), 125);
-	
-	graphics_context_set_fill_color(ctx, GColorBlack);
-	graphics_fill_circle(ctx, grect_center_point(&me->frame), 47);
-	gpath_draw_filled(ctx, &hour_overlay_path);
-}
-
-void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
-	if (t->tick_time->tm_sec == 0) {
-		layer_mark_dirty(&minute_overlay_layer);
+static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
+	layer_mark_dirty(minute_overlay_layer);
+	layer_mark_dirty(hour_overlay_layer);
+	/*
+	if (tick_time->tm_sec == 0) {
+		layer_mark_dirty(minute_overlay_layer);
 		
-		if (t->tick_time->tm_min % 2 == 0) {
-			layer_mark_dirty(&hour_overlay_layer);
+		if (tick_time->tm_min % 2 == 0) {
+			layer_mark_dirty(hour_overlay_layer);
 		}
 	}
+	*/
 }
 
-void display_init(AppContextRef *ctx) {
-	window_init(&window, "Mask");
-	window_stack_push(&window, true);
+static void minute_display_layer_update_callback(Layer *layer, GContext* ctx) {
+	time_t now = time(NULL);
+	struct tm *t = localtime(&now);
 	
-	init_layer_path_and_center(&hour_overlay_layer, &hour_overlay_path, &HOUR_OVERLAY_POINTS, &hour_display_layer_update_callback);
-	layer_add_child(&window.layer, &hour_overlay_layer);
+	GRect bounds = layer_get_bounds(layer);
+	GPoint center = grect_center_point(&bounds);
+
+	unsigned int angle = ((t->tm_min - (t->tm_min % 5)) * 6);
+	gpath_rotate_to(minute_overlay_path, (TRIG_MAX_ANGLE / 360) * angle);
 	
-	init_layer_path_and_center(&minute_overlay_layer, &minute_overlay_path, &MINUTE_OVERLAY_POINTS, &minute_display_layer_update_callback);
-	layer_add_child(&window.layer, &minute_overlay_layer);
-	
-	bmp_init_container(RESOURCE_ID_IMAGE_NUMBERS, &numbers_image);
-	bitmap_layer_set_compositing_mode(&numbers_image.layer, GCompOpOr);
-	layer_add_child(&window.layer, &numbers_image.layer.layer);
+	graphics_context_set_fill_color(ctx, GColorWhite);
+	graphics_fill_circle(ctx, center, 44);
+	graphics_context_set_fill_color(ctx, GColorBlack);
+	gpath_draw_filled(ctx, minute_overlay_path);
 }
 
-void handle_init(AppContextRef ctx) {
-	resource_init_current_app(&APP_RESOURCES);
+static void hour_display_layer_update_callback(Layer *layer, GContext* ctx) {
+	time_t now = time(NULL);
+	struct tm *t = localtime(&now);
 	
-	AnalogueGRect = GRect(0, 0, 144, 168);
+	GRect bounds = layer_get_bounds(layer);
+	GPoint center = grect_center_point(&bounds);
+
+	unsigned int angle = (t->tm_hour * 30);
+	gpath_rotate_to(hour_overlay_path, (TRIG_MAX_ANGLE / 360) * angle);
 	
-	display_init(&ctx);
-	
-	layer_mark_dirty(&minute_overlay_layer);
-	layer_mark_dirty(&hour_overlay_layer);
+	graphics_context_set_fill_color(ctx, GColorWhite);
+	graphics_fill_circle(ctx, center, 125);
+	graphics_context_set_fill_color(ctx, GColorBlack);
+	gpath_draw_filled(ctx, hour_overlay_path);
+	graphics_fill_circle(ctx, center, 47);
 }
 
-void handle_deinit(AppContextRef ctx) {
-	bmp_deinit_container(&numbers_image);
+static void init() {
+	window = window_create();
+	window_set_background_color(window, GColorBlack);
+	window_stack_push(window, true);
+	
+	Layer *window_layer = window_get_root_layer(window);
+	GRect bounds = layer_get_bounds(window_layer);
+	
+	// Create hour path
+	hour_overlay_layer = layer_create(bounds);
+	layer_set_update_proc(hour_overlay_layer, hour_display_layer_update_callback);
+	layer_add_child(window_layer, hour_overlay_layer);
+	hour_overlay_path = gpath_create(&HOUR_OVERLAY_POINTS);
+	gpath_move_to(hour_overlay_path, grect_center_point(&bounds));
+	
+	// Create minute path
+	minute_overlay_layer = layer_create(bounds);
+	layer_set_update_proc(minute_overlay_layer, minute_display_layer_update_callback);
+	layer_add_child(window_layer, minute_overlay_layer);
+	minute_overlay_path = gpath_create(&MINUTE_OVERLAY_POINTS);
+	gpath_move_to(minute_overlay_path, grect_center_point(&bounds));
+	
+	// Create background image
+	numbers_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NUMBERS);
+	numbers_layer = bitmap_layer_create(layer_get_frame(window_layer));
+	bitmap_layer_set_bitmap(numbers_layer, numbers_image);
+	bitmap_layer_set_compositing_mode(numbers_layer, GCompOpOr);
+	layer_add_child(window_layer, bitmap_layer_get_layer(numbers_layer));
+	
+//	tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+	tick_timer_service_subscribe(SECOND_UNIT, handle_minute_tick);
 }
 
-void pbl_main(void *params) {
-	PebbleAppHandlers handlers = {
-		.init_handler = &handle_init,
-		.deinit_handler = &handle_deinit,
-		.tick_info = {
-			.tick_handler = &handle_minute_tick,
-			.tick_units = MINUTE_UNIT
-		}
-	};
-	
-	app_event_loop(params, &handlers);
+static void deinit() {
+	window_destroy(window);
+	layer_destroy(minute_overlay_layer);
+	layer_destroy(hour_overlay_layer);
+	gpath_destroy(minute_overlay_path);
+	gpath_destroy(hour_overlay_path);
+	gbitmap_destroy(numbers_image);
+	bitmap_layer_destroy(numbers_layer);
+}
+
+int main(void) {
+	init();
+	app_event_loop();
+	deinit();
 }
