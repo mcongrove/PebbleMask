935825fd21d30a883e99919daa1326e823223942
Matthew Congrove <mattcongrove@gmail.com>
2014-01-08 18:24:35 -0500

    Adding version info

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -5,8 +5,8 @@ Pebble.addEventListener("ready", function(e) {
 Pebble.addEventListener("showConfiguration", function(e) {
 	var options = JSON.parse(window.localStorage.getItem("options"));
 	
-	var url = "http://wearewearable.com/mask/?" +
-			  "theme=" + encodeURIComponent(options["0"]);
+	var url = "http://wearewearable.com/mask/?v=2.0" +
+			  "&theme=" + encodeURIComponent(options["0"]);
 	
 	Pebble.openURL(url);
 });
